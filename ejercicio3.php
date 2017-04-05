81c1a4a4bccb4955b64a43f370f5510c328a4c33
solera <solera@10.2.1.133>
2017-04-05 20:23:42 +0200

    Añadidos ejercicios impares del 1 al 10.

---
+++ ejercicio3.php
@@ -0,0 +1,13 @@
+<?php
+
+$primera = 10;
+$segunda = 2;
+
+
+$resta = $primera - $segunda;
+
+echo "La resta de $primera y $segunda es $resta ."."<br>";
+
+$division = $primera / $segunda;
+
+echo "La division de $primera y $segunda es $division ."."<br>";
