e48f24717b5554768cf1e0e1cf0e86f3c1917346
anfer1095 <anfer1095@INF2-PC12>
2017-04-05 20:26:24 +0200

    Añadidos ejercicios pares del 1 al 10.

---
+++ Boletin1/Ejercicio 4.php
@@ -0,0 +1,10 @@
+<?php
+  $primera = 4;
+  $segunda = 8;
+  $elevado = pow($primera,$segunda);
+  $raizcuadrada = sqrt($primera);
+  $raizcuadrada1 = sqrt($segunda);
+  echo "$primera elevada a $segunda es $elevado <br>";
+  echo "Raiz cuadrada de $primera es $raizcuadrada <br>";
+  echo "Rai­z cuadrada de $segunda es $raizcuadrada1";
+?>
